cdd1622c67f824fedcef1504e000df340f912b39
Attila Kovacs <motching@gmail.com>
2016-08-29 15:53:24 -0700

    load CSVs into memory

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ lazy val root = (project in file(".")).
     name := "noredink",
     version := "0.1",
     scalaVersion := "2.11.7",
-    scalacOptions ++= Seq("-deprecation", "-feature"),
-    libraryDependencies += "com.github.scala-incubator.io" % "scala-io-file_2.11" % "0.4.3-1"
+    scalacOptions ++= Seq("-deprecation", "-feature")
   )
+
+connectInput in run := true
--- src/main/scala/noredink.scala
+++ src/main/scala/noredink.scala
@@ -1,9 +1,37 @@
+import scala.io.{StdIn, Source}
+
 package object noRedInk {
 
   def main(args: Array[String]): Unit = {
 
+    //val numOfQuestions:Int = StdIn.readLine("Number of wanted questions:  ").toInt
+    //println("nums: " + numOfQuestions.toString)
+
+    val questions = Source
+      .fromURL(getClass.getClassLoader.getResource("questions.csv"))
+      .getLines
+      .toList
+      .tail
+
+    val usage = Source
+      .fromURL(getClass.getClassLoader.getResource("usage.csv"))
+      .getLines
+      .toList
+      .tail
+
+    val lastRow = questions
+      .last
+      .split(",")
+      .toList
+
+    val numOfStrands = lastRow(0).toInt
+    val numOfStandards = lastRow(2).toInt
+
+    println("strands " + numOfStrands)
+    println("standards " + numOfStandards)
+
     val t0 = System.nanoTime()
-    println("I work!")
+
     val t1 = System.nanoTime()
     println("Elapsed time: " + (t1 - t0)/1000 + "us")
 
